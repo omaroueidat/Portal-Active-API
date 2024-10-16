53da38ec367f3ce1e5f2e2778a7a45501da74a36
Omar Oueidat <3omaroueidat4321@gmail.com>
2024-10-17 01:30:37 +0300

    first commit

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using Domain;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivitiesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ActivitiesController(DataContext context)
+        {
+            _context = context; 
+        }
+
+        [HttpGet] // api/activities
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+        public async Task<ActionResult<Activity>> GetActivity(Guid Id)
+        {
+            return await _context.Activities.SingleOrDefaultAsync(ac => ac.Id == Id);
+        }
+
+    }
+}
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Domain.Activity> Activities { get; set; }
+    }
+}
